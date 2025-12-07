730f5efbab42cfd47881e5bb69ebf3491dc7ff53
theppl <polimera0000@gmail.com>
2025-12-07 16:37:19 +0530

    Switch DB2

--- marathon/settings.py
+++ marathon/settings.py
@@ -62,18 +62,22 @@ WSGI_APPLICATION = 'marathon.wsgi.application'
 
 # ---------------- DATABASE ---------------- #
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    #import dj_database_url  # or keep this at the top of the file
+
+    DATABASES["default"] = dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True,  # Render Postgres uses SSL
+    )
 
 
 # ---------------- AUTH VALIDATORS ---------------- #
